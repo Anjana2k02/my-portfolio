cf8c88e7e7887e0f0c8a338555b0ab0f506049ea
Anjana2k02 <101281237+Anjana2k02@users.noreply.github.com>
2024-07-15 21:02:17 +0530

    done 70%

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import "./app.scss"
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+
+
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+     <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+      </section>
+    <section id="Services"><Parallax type="services"/></section>
+    
+    <Portfolio/>
+    <section id="Contact"><Contact/></section>
+    
+    </div>;
 };
 
-export default App;
+export default App
